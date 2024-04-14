df51002cc0d2ba856b229388c554bb44362c2b67
anonfedora <musaeleazar090@gmail.com>
2024-04-14 19:46:44 +0100

    components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Providers } from "@/components/redux/provider";
 
@@ -18,7 +19,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <Providers>{children}</Providers>
+                <Providers>
+                    <Navbar />
+                    {children}
+                </Providers>
             </body>
         </html>
     );
