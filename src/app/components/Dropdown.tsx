3b50d0618ccb22807a1211f56167942dd3321906
anonfedora <musaeleazar090@gmail.com>
2024-04-14 20:12:33 +0100

    components

---
+++ src/app/components/Dropdown.tsx
@@ -0,0 +1,21 @@
+interface IDropdown {
+    show: boolean;
+}
+
+export default function Dropdown({ show }: IDropdown) {
+    return (
+        <div
+            className={`${
+                show ? "block" : "hidden"
+            } w-48 absolute top-full bg-white
+         border shadow-lg right-0 py-2 rounded-2xl`}
+        >
+            <div className="hover:bg-gray-300">
+                <button className="text-sm px-4 py-2">Edit Board</button>
+            </div>
+            <div className="hover:bg-gray-300">
+                <button className="text-sm px-4 py-2">Delete Board</button>
+            </div>
+        </div>
+    );
+}
